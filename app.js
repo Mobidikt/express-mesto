0c656db0db7f9e64b1d9cfab0b868d354649195a
Kirill Mecker <ghost3299@yandex.ru>
2020-11-03 19:47:08 +0400

    feat:models user and card

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const routes = require('./routes/index.js');
+
+
 const { PORT = 3000 } = process.env;
 const app = express();
+
+const mongoDbUrl = 'mongodb://localhost:27017/mestodb';
+const mongoConnectOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
+
+mongoose.connect(mongoDbUrl, mongoConnectOptions)
+  .then(() => {
+    console.log('База данных подключена');
+  })
+  .catch((err) => {
+    console.log(`Ошибка при подключении базы данных: ${err}`);
+  });
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
